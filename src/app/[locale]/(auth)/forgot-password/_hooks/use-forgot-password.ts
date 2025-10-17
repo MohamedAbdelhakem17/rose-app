7e4b9fb2baa34c0a0a9c6c4d13ad0c263222401e
OmarQussas <omarqussas@gmail.com>
2025-10-18 00:20:25 +0300

    Fix/ duplicated success message

--- src/app/[locale]/(auth)/forgot-password/_hooks/use-forgot-password.ts
+++ src/app/[locale]/(auth)/forgot-password/_hooks/use-forgot-password.ts
@@ -2,31 +2,14 @@ import { useMutation } from '@tanstack/react-query';
 import { toast } from 'sonner';
 import { forgotPasswordAction } from '@/lib/actions/forgot-password-action';
 import { ForgotPasswordResponse } from '@/lib/types/auth';
-import useCookie from '@/hooks/use-cookie';
 
 export function useForgotPassword() {
-  // initialize cookie hook
-  const { setCookieValue } = useCookie({
-    name: 'reset_email',
-    expireInMinutes: 15, // cookie expires in 15 minutes
-    allowOverwrite: true,
-  });
-
   // main mutation
   const mutationFn = async (email: string): Promise<ForgotPasswordResponse> => {
     return await forgotPasswordAction({ email });
   };
   return useMutation<ForgotPasswordResponse, Error, string>({
     mutationFn,
-
-    onSuccess: (data, email) => {
-      // store the email in cookies
-      setCookieValue(email);
-
-      // show success message
-      toast.success(data.info || 'Email sent successfully!');
-      console.log('Forgot Password Success:', data);
-    },
 
     onError: error => {
       toast.error(error?.message || 'Something went wrong');
--- src/app/[locale]/(auth)/forgot-password/page.tsx
+++ src/app/[locale]/(auth)/forgot-password/page.tsx
@@ -1,24 +1,6 @@
-'use client';
 import ForgotPasswordLayout from './_components/_layout/forgot-password-layout';
 
-'use client';
-import React from 'react';
-import { Section } from '@/components/layout';
-import { useForgotPassword } from './_hooks/use-forgot-password';
-import { useResetPassword } from './_hooks/use-reset-password';
-import AuthenticationHeading from '../_components/_layout/authentication-heading';
-import { ForgotPasswordForm } from './_components/steps/forgot-password-step';
-import { ResetPasswordForm } from './_components/steps/reset-password-step';
-import { AuthRedirect } from './_components/_shared/auth-redirect';
-import { useTranslations } from 'next-intl';
-
 export default function ForgotPassword() {
-  const t = useTranslations();
-
-  const { mutate: forgotMutate, isPending: isForgotPending } =
-    useForgotPassword();
-  const { mutate: resetMutate, isPending: isResetPending } = useResetPassword();
-
   return (
     <main>
       {/* Render current step of forgot password steps */}
